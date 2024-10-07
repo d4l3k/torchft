da793390a5743fe632d16ca3c53494b0f2b7562f
Tristan Rice <rice@fn.lc>
2024-10-07 09:41:02 -0700

    checkpointing: added mismatch test

--- torchft/checkpointing_test.py
+++ torchft/checkpointing_test.py
@@ -1,5 +1,7 @@
 from torchft.checkpointing import CheckpointServer
 
+import urllib.error
+
 from unittest import TestCase
 from unittest.mock import MagicMock
 
@@ -11,9 +13,18 @@ class TestCheckpointing(TestCase):
         state_dict_fn.return_value = expected
         server = CheckpointServer(state_dict=state_dict_fn)
 
+        server.disallow_checkpoint()
+        server.allow_checkpoint(1234)
+
         addr = server.address()
 
         out = CheckpointServer.load_from_address(addr)
         self.assertEqual(out, expected)
+
+        # test mismatch case
+        server.allow_checkpoint(2345)
+
+        with self.assertRaisesRegex(urllib.error.HTTPError, r"Error 400"):
+            CheckpointServer.load_from_address(addr)
 
         server.shutdown()
