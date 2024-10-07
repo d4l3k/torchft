b98f8ea3e9a09eeb1fb7899b50ac2b4bc9afb5d3
Tristan Rice <rice@fn.lc>
2024-10-07 03:49:19 -0700

    train: removed unnecessary allreduces

--- train.py
+++ train.py
@@ -28,16 +28,15 @@ for i in range(1000):
     batch = torch.rand(2, 2, device=device)
 
     # must be called at the beginning of each train loop
+    # Quorum computation is triggered here but only needed in the backwards pass.
     optimizer.zero_grad()
 
     out = m(batch)
     loss = out.sum()
 
+    # Gradient all reduce overlaps with the backwards pass.
     loss.backward()
 
-    for p in m.parameters():
-        if p.grad is not None:
-            manager.allreduce_grad(p.grad)
-
     # must be called at the end of the train loop
+    # This may not actually step the optimizer if an error occured during grad allreduce.
     optimizer.step()
