e123902be446cc96830fb4a59d3b9144c209eb6c
Tristan Rice <rice@fn.lc>
2024-10-12 21:21:08 -0700

    publish: added relevant python versions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,9 @@
 set -ex
 
 docker build -t torchft .
-docker run --rm -v $(pwd):/io torchft build --release
+docker run --rm -v $(pwd):/io torchft build --release --interpreter python3.12
+docker run --rm -v $(pwd):/io torchft build --release --interpreter python3.11
+docker run --rm -v $(pwd):/io torchft build --release --interpreter python3.10
+docker run --rm -v $(pwd):/io torchft build --release --interpreter python3.9
+docker run --rm -v $(pwd):/io torchft build --release --interpreter python3.8
 python3 -m twine upload target/wheels/*manylinux2014*
